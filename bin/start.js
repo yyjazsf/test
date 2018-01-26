43fb6b0a53c721746f90ff314a2954413518e288
yingyujia <yyjazsf@live.cn>
2018-01-26 11:06:07 +0800

    add ecmascript test

--- bin/start.js
+++ bin/start.js
@@ -7,7 +7,7 @@ const config = require('./bs-config')
 config.server = {
   baseDir: process.argv[2] || './',
   routes: {
-    "/xx": "node_modules"  //
+    "/xx": "../node_modules"  //
   },
 }
 config.files = `${config.server.baseDir}/**`
